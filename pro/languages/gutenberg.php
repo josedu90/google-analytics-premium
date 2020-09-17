dd176410cf41c1735147a10821cf936713ce3f3f
josedu90 <jose_edubg@hotmail.com>
2020-09-17 03:38:47 -0500

    up

---
+++ pro/languages/gutenberg.php
@@ -0,0 +1,179 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelCharacterCount.js:10
+	__( 'Character Count', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelCharacterCount.js:36
+	__( 'Too Short', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelCharacterCount.js:37
+	__( 'You have space to add more keywords and power words to boost your rankings and click-through rate.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelCharacterCount.js:39
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:26
+	__( 'Good', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelCharacterCount.js:40
+	__( 'Headlines that are about 55 characters long will display fully in search results and tend to get more clicks.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelCharacterCount.js:42
+	__( 'Too Long', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelCharacterCount.js:43
+	__( 'At this length, it will get cut off in search results. Try reducing it to about 55 characters.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelNewScore.js:10
+	__( 'New Score', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelNewScore.js:11
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineTabCurrentScore.js:11
+	__( 'A good score is between 40 and 60. For best results, you should strive for 70 and above.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelNewScore.js:12
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineSwitcher.js:10
+	__( 'Current Score', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelPreviousScores.js:8
+	__( 'Previous Scores', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSearchPreview.js:7
+	__( 'Search Preview', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSearchPreview.js:8
+	__( 'Here is how your headline will look like in google search results page.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:10
+	__( 'Headlines that are strongly positive or negative tend to get more engagement then neutral ones.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:11
+	__( 'Your headline has a positive sentiment.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:12
+	__( 'Positive headlines tend to get better engagement than neutral or negative ones.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:13
+	__( 'Your headline has a negative sentiment.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:14
+	__( 'Negative headlines are attention-grabbing and tend to perform better than neutral ones.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:15
+	__( 'Sentiment', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:17
+	__( 'Neutral', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:18
+	__( 'Positive', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:18
+	__( 'Negative', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelSentiment.js:9
+	__( 'Your headline has a neutral sentiment.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelStartEndWords.js:6
+	__( 'Beginning & Ending Words', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelStartEndWords.js:8
+	__( 'Most readers only look at the first and last 3 words of a headline before deciding whether to click.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelType.js:12
+	__( 'Headlines that are lists and how-to get more engagement on average than other types of headlines. %1sRead more about %2sdifferent types of headlines here.%3s', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelType.js:7
+	__( 'Headline Type', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:10
+	__( 'Word Balance', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:11
+	__( 'Compare the percentages of your results to the goal for each category and adjust as necessary.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:16
+	__( 'Your headline would be more likely to get clicks if it had more uncommon words.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:19
+	__( 'Headlines with 20-30% common words are more likely to get clicks.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:24
+	__( 'Headlines with uncommon words are more likely to get clicks.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:27
+	__( 'Emotionally triggered headlines are likely to drive more clicks.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:30
+	__( 'Headlines with Power Words are more likely to get clicks.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:31
+	__( 'Common Words', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:32
+	__( '20-30%', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:33
+	__( 'Uncommon Words', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:34
+	__( '10-20%', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:35
+	__( 'Emotional Words', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:36
+	__( '10-15%', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:37
+	__( 'Power Words', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordBalance.js:38
+	__( 'At least one', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:10
+	__( 'Word Count', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:21
+	__( 'Not Enough Words', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:22
+	__( 'Your headline doesn’t use enough words. You have more space to add keywords and power words to improve your SEO and get more engagement.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:27
+	__( 'Your headline has the right amount of words. Headlines are more likely to be clicked on in search results if they have about 6 words.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:31
+	__( 'Reduce Word Count', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:35
+	__( 'Too Many Words', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlinePanelWordCount.js:36
+	__( 'Your headline has too many words. Long headlines will get cut off in search results and won’t get as many clicks.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineSwitcher.js:11
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineTabNewScore.js:8
+	__( 'Try New Headline', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineTabCurrentScore.js:10
+	__( 'Score', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineTabNewScore.js:10
+	__( 'Analyze', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineTabNewScore.js:9
+	__( 'Enter a different headline than your post title to see how it compares.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/components/HeadlineWordsBlock.js:6
+	__( 'Goal: ', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/index.js:21
+	__( 'Headline Analyzer', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/index.js:22
+	__( 'Write your post title to see the analyzer data. This Headline Analyzer tool enables you to write irresistible SEO headlines that drive traffic, shares, and rank better in search results.', 'ga-premium' ),
+
+	// Reference: src/plugins/monsterinsights-Headline-Analyzer/index.js:27
+	__( 'This headline analyzer is part of MonsterInsights to help you increase your traffic. See your %1swebsite traffic reports%2s.', 'ga-premium' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
